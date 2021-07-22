623c03af051afc27a570929332f4be9b7c6097dd
Bryce Campbell <tonyhawk2100@gmail.com>
2021-07-21 17:23:42 -0700

    work on retrieving previous records.

--- src/records.rs
+++ src/records.rs
@@ -5,7 +5,37 @@ pub struct Records {
 }
 
 impl Records {
-    fn from_file(path: &str) -> Result<Records, String> {
+    pub fn from_file(path: &str) -> Result<Records, String> {
+        match Record::from_file(path) {
+            Ok(records) => Ok(Records { records }),
+            Err(error) => Err(error)
+        }
+    }
+    
+    pub fn sorted_records(&self) -> Vec<Record> {
+        let mut stored_records = self.records.clone();
+        stored_records.sort_by_key(|r| r.transaction.date);
+        
+        stored_records
+    }
 
+    fn record_before(&self, record: &Record) -> Option<Record> {
+        if let Some(index) = self.sorted_records().iter().position(|r| r == record) {
+            if index > 0 { Some(self.sorted_records()[index-1].clone()) } else { None }
+        } else {
+            None
+        }
+    }
+
+    fn retrieve_previous_records_for_records(&mut self) {
+        let mut stored_records = self.records.clone();
+
+        for record in stored_records.iter_mut() {
+            if let Some(previous_record) = self.record_before(&record) {
+                record.previous_record = Some(Box::new(previous_record))
+            }
+        }
+
+        self.records = stored_records;
     }
 }
