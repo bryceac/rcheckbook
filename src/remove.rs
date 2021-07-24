8ef526ac8e0ae002f4768f9267b0ce93bd30e94e
Bryce Campbell <tonyhawk2100@gmail.com>
2021-07-24 09:56:16 -0700

    make sure remove is loose on path specification.

--- src/remove.rs
+++ src/remove.rs
@@ -13,6 +13,23 @@ pub struct Remove {
 
 impl Remove {
     pub fn run(&self) -> Result<(), String> {
+        if self.file_path.starts_with("~") {
+            let modified_path = shellexpand::tilde(&self.file_path).into_owned();
+    
+            self.remove_record(&modified_path)
+        } else {
+            match fs::canonicalize(self.file_path.clone()) {
+                Ok(real_path) => if let Some(file_path) = real_path.to_str() {
+                    self.remove_record(file_path)
+                } else {
+                    Err(String::from("File path could not be recognized"))
+                },
+                Err(error) => Err(error.to_string())
+            }
+        }
+    }
+
+    fn remove_record(&self, p: &str) -> Result<(), String> {
         let mut stored_records = Records::from_file(&self.file_path)?;
 
         stored_records.remove(self.id.clone());
