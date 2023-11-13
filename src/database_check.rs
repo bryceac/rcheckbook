bc136103f1dda906563d9cd06a95d1d6c2ff9fb8
Bryce Campbell <tonyhawk2100@gmail.com>
2023-11-13 14:44:56 -0800

    keep working on new module.

--- src/database_check.rs
+++ src/database_check.rs
@@ -1,6 +1,8 @@
-fn copy_database_if_not_exists() {
+use std::{ fs, path::Path };
+
+pub fn copy_database_if_not_exists(p: &str) {
     if self.file_path.starts_with("~") {
-        let modified_path = shellexpand::tilde(&self.file_path).into_owned();
+        let modified_path = shellexpand::tilde(p).into_owned();
 
         if !Path::new(&modified_path).exists() {
             let target_path = Path::new(&modified_path);
@@ -9,7 +11,7 @@ fn copy_database_if_not_exists() {
             fs::copy(original_path, target_path);
         } else {}
     } else {
-        if let Ok(real_path) = fs::canonicalize(self.file_path.clone()) {
+        if let Ok(real_path) = fs::canonicalize(p.clone()) {
             if let Some(file_path) = real_path.to_str() {
                 let target_path = Path::new(&file_path);
                 let original_path = Path::new("register.db");
