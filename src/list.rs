928a31b4f4f9d409a71686b6352b1fc08691f6c4
Bryce Campbell <tonyhawk2100@gmail.com>
2023-11-13 14:32:37 -0800

    work on getting application to deal with database.

--- src/list.rs
+++ src/list.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use std::fs;
+use std::{ fs, path::Path };
 use crate::records::Records;
 
 #[derive(Parser)]
@@ -29,6 +29,28 @@ impl List {
             },
             Err(error) => {
                 println!("{}", error)
+            }
+        }
+    }
+
+    fn copy_database_if_not_exists() {
+        if self.file_path.starts_with("~") {
+            let modified_path = shellexpand::tilde(&self.file_path).into_owned();
+
+            if !Path::new(&modified_path).exists() {
+                let target_path = Path::new(&modified_path);
+                let original_path = Path::new("register.db");
+
+                fs::copy(original_path, target_path);
+            } else {}
+        } else {
+            if let Ok(real_path) = fs::canonicalize(self.file_path.clone()) {
+                if let Some(file_path) = real_path.to_str() {
+                    let target_path = Path::new(&file_path);
+                    let original_path = Path::new("register.db");
+
+                    fs::copy(original_path, target_path);
+                }
             }
         }
     }
