61284ad08ae67c333fbbb7c70228341c3304d5a4
Bryce Campbell <tonyhawk2100@gmail.com>
2023-11-13 21:19:37 -0800

    work on trying to retrieve data.

--- src/list.rs
+++ src/list.rs
@@ -1,6 +1,8 @@
 use clap::Parser;
 use crate::records::Records;
+use bcheck::{ Record, Transaction };
 use crate::database::copy_database_if_not_exists;
+use rusqlite::Connection;
 
 #[derive(Parser)]
 pub struct List {
@@ -23,6 +25,7 @@ impl List {
             }
         } */
         copy_database_if_not_exists(&self.file_path);
+        self.load_from_db(&self.file_path);
     }
 
     fn load_from_db(&self, p: &str) {
@@ -35,7 +38,15 @@ impl List {
             }
         } */
 
-
+        match Connection::open(p) {
+            Ok(db) => {
+                if let Ok(statement) = db.prepare("SELECT * from ledger") {
+                    let record_query = statement.query_map([], |row| Record::from(row.get(0).unwrap(), Transaction::from(row.get(1), row.get(2), row.get(3), row.get(4), row.get(5), row.get(6), row.get(7))));
+                }
+                let _ = Connection::close(db);
+            },
+            _ => println!("Could not connect")
+        }
     }
 }
 
