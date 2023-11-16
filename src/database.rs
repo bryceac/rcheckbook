4e8ad9a63fa8847ad1be61279ff2a0222140ff80
Bryce Campbell <tonyhawk2100@gmail.com>
2023-11-16 14:26:50 -0800

    remove code that was commented out.

--- src/database.rs
+++ src/database.rs
@@ -4,29 +4,6 @@ use bcheck::{ Record, Transaction, TransactionType };
 use crate::shared::*;
 
 pub fn copy_database_if_not_exists(p: &str) {
-    /* if p.starts_with("~") {
-        let modified_path = shellexpand::tilde(p).into_owned();
-
-        if !Path::new(&modified_path).exists() {
-            let target_path = Path::new(&modified_path);
-            let original_path = Path::new("register.db");
-
-            let _ = fs::create_dir_all(&target_path.parent().unwrap());
-
-            let _ = fs::copy(original_path, target_path);
-        } else {}
-    } else {
-        if let Ok(real_path) = fs::canonicalize(p) {
-
-            if !real_path.exists() {
-                let original_path = Path::new("register.db");
-
-                let _ = fs::create_dir_all(real_path.as_path().parent().unwrap());
-
-                let _ = fs::copy(original_path, real_path);
-            } else {}
-        }
-    } */
     let target_path= real_path(p);
     let original_path = Path::new("register.db");
 
