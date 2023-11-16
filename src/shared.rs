cc474ba629993654cb1b76584cb99b1f206e7639
Bryce Campbell <tonyhawk2100@gmail.com>
2023-11-16 14:17:19 -0800

    create function to retrieve path.

--- src/shared.rs
+++ src/shared.rs
@@ -1,1 +1,9 @@
 use std::{ fs, path::Path };
+
+pub fn real_path(p: &str) -> String {
+    if p.starts_with("~") {
+        shellexpand::tilde(p).into_owned()
+    } else {
+        fs::canonicalize(p).unwrap().as_path().to_str().unwrap().to_owned()
+    }
+}
