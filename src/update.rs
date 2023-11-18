cf5cbf35c4a6d6532a316402b59023d07d971979
Bryce Campbell <tonyhawk2100@gmail.com>
2023-11-18 14:29:37 -0800

    remove commented code.

--- src/update.rs
+++ src/update.rs
@@ -60,6 +60,14 @@ impl Update {
                 stored_record.transaction.check_number = Some(check_number);
             }
 
+            if let Some(category) = &self.category {
+                stored_record.transaction.category = if category.is_empty() { 
+                    None 
+                } else {
+                    Some(category.to_owned())
+                };
+            }
+
             if let Some(vendor) = &self.vendor {
                 stored_record.transaction.vendor = vendor.to_owned();
             }
@@ -81,50 +89,8 @@ impl Update {
             } else if self.not_reconciled {
                 stored_record.transaction.is_reconciled = false
             } else {}
+
+            update_record_in_db(p, &stored_record);
         }
-
-        /* if let Some(mut record) = stored_records.record_matching_id(self.id.clone()) {
-            if let Some(date_string) = self.date.clone() {
-                if let Ok(datetime) = date_string.local_datetime() {
-                    record.transaction.date = datetime
-                }
-            } else {}
-
-            if let Some(check_number) = self.check_number {
-                record.transaction.check_number = Some(check_number);
-            } else {}
-            
-            if let Some(vendor) = self.vendor.clone() {
-                record.transaction.vendor = vendor;
-            } else {}
-
-            if let Some(memo) = self.memo.clone() {
-                record.transaction.memo = memo;
-            } else {}
-
-            if let Some(amount) = self.amount {
-                record.transaction.amount = OrderedFloat(amount);
-            } else {}
-
-            if let Some(transaction_type) = self.transaction_type.clone() {
-                record.transaction.transaction_type = transaction_type;
-            } else {}
-
-            if self.reconciled {
-                record.transaction.is_reconciled = true;
-            } else if self.not_reconciled {
-                record.transaction.is_reconciled = false;
-            } else {}
-
-            stored_records.update(self.id.clone(), record);
-
-            match stored_records.sorted_records().save(p) {
-                Ok(()) => Ok(()),
-                Err(error) => Err(error.to_string())
-            }
-
-        } else {
-            Err(String::from("Could not find record."))
-        } */
     }
 }
