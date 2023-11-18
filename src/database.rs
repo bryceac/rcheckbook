9d969b2a841463a5efcd740cb7a53238d4a4f294
Bryce Campbell <tonyhawk2100@gmail.com>
2023-11-18 13:23:42 -0800

    readd record variable.

--- src/database.rs
+++ src/database.rs
@@ -199,12 +199,12 @@ pub fn update_record_in_db(p: &str, r: &Record) {
         None
     };
 
-    if let Some(_) = retrieve_record_with_id_from_db(p, &r.id) {
+    if let Some(record) = retrieve_record_with_id_from_db(p, &r.id) {
         if let Ok(db) = Connection::open(p) {
             let update_statement = format!("UPDATE trades SET date = (?1), check_number = (?2), vendor = (?3), memo = (?4), amount = (?5), category = (?6), reconciled = (?7) WHERE id = (?8)");
 
             if let Ok(mut statement) = db.prepare(&update_statement) {
-                if let Err(error) = statement.execute(params![format!("{}", r.transaction.date.format("%Y-%m-%d")), r.transaction.check_number, r.transaction.vendor, r.transaction.memo, if let TransactionType::Deposit = r.transaction.transaction_type { r.transaction.amount.into_inner() } else { r.transaction.amount.into_inner()*-1.0 }, category_id, r.transaction.is_reconciled, r.id.to_uppercase()]) {
+                if let Err(error) = statement.execute(params![format!("{}", r.transaction.date.format("%Y-%m-%d")), r.transaction.check_number, r.transaction.vendor, r.transaction.memo, if let TransactionType::Deposit = r.transaction.transaction_type { r.transaction.amount.into_inner() } else { r.transaction.amount.into_inner()*-1.0 }, category_id, r.transaction.is_reconciled, record.id]) {
                     println!("{}", error);
                 }
             }
