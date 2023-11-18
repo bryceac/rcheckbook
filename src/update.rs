5d9bbf1fd4aaa5e16a89ec3f08fa5e5890d9b6e5
Bryce Campbell <tonyhawk2100@gmail.com>
2023-11-18 14:19:42 -0800

    continue working on gathering data for record.

--- src/update.rs
+++ src/update.rs
@@ -1,4 +1,4 @@
-use bcheck::LocalDateTimeStringExt;
+use bcheck::{ LocalDateTimeStringExt, TransactionType, OrderedFloat };
 use crate::{database::*, shared::* };
 use clap::Parser;
 use std::fs;
@@ -43,8 +43,42 @@ impl Update {
 
     fn update_record(&self, p: &str) {
 
-        if let Some(stored_record) = retrieve_record_with_id_from_db(p, &self.id) {
-            let date_string = 
+        if let Some(mut stored_record) = retrieve_record_with_id_from_db(p, &self.id) {
+            if let Some(date) = &self.date {
+                match date.local_datetime() {
+                    Ok(new_date) => stored_record.transaction.date = new_date,
+                    Err(error) => {
+                        println!("{}", error);
+                        return;
+                    }
+                }
+            }
+
+            if let Some(check_number) = self.check_number {
+                stored_record.transaction.check_number = Some(check_number);
+            }
+
+            if let Some(vendor) = &self.vendor {
+                stored_record.transaction.vendor = vendor.to_owned();
+            }
+
+            if let Some(memo) = &self.memo {
+                stored_record.transaction.memo = memo.to_owned();
+            }
+
+            if let Some(amount) = self.amount {
+                stored_record.transaction.amount = OrderedFloat(amount);
+            }
+
+            if let Some(transaction_type) = self.transaction_type.clone() {
+                stored_record.transaction.transaction_type = transaction_type;
+            }
+
+            if self.reconciled {
+                stored_record.transaction.is_reconciled = true
+            } else if self.not_reconciled {
+                stored_record.transaction.is_reconciled = false
+            } else {}
         }
 
         /* if let Some(mut record) = stored_records.record_matching_id(self.id.clone()) {
