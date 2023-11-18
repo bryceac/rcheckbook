c3ae60d9d511e6efad215683c6be3e9eb8c5202b
Bryce Campbell <tonyhawk2100@gmail.com>
2023-11-18 11:12:40 -0800

    remove unused variable.

--- src/database.rs
+++ src/database.rs
@@ -199,7 +199,7 @@ pub fn update_record_in_db(p: &str, r: &Record) {
         None
     };
 
-    if let Some(record) = retrieve_record_with_id_from_db(p, &r.id) {
+    if let Some(_) = retrieve_record_with_id_from_db(p, &r.id) {
         if let Ok(db) = Connection::open(p) {
             let update_statement = format!("UPDATE trades SET date = (?1), check_number = (?2), vendor = (?3), memo = (?4), amount = (?5), category = (?6), reconciled = (?7)");
 
