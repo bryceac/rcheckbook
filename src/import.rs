29efcd8e62681810d24e0355bcb52eeb05d68fb5
Bryce Campbell <tonyhawk2100@gmail.com>
2023-11-18 17:32:22 -0800

    add new module for subcommand.

---
+++ src/import.rs
@@ -0,0 +1,20 @@
+use clap::Parser;
+use crate::records::Records;
+use crate::database::*;
+
+
+#[derive(Parser)]
+pub struct List {
+
+    #[clap(default_value = "~/.checkbook/register.db")]
+    pub file_path: String,
+}
+
+impl List {
+    pub fn run(&self) {
+        copy_database_if_not_exists(&self.file_path);
+        let mut record_store = Records::from(load_records_from_db(&self.file_path));
+        record_store.display(&self.file_path);
+    }
+}
+
