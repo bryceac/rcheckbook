958566de58341622a84d7789acf86e2a62398085
Bryce Campbell <tonyhawk2100@gmail.com>
2023-11-18 18:07:27 -0800

    add module to allow exporting data.

---
+++ src/export.rs
@@ -0,0 +1,45 @@
+use clap::Parser;
+use crate::{ database::*, shared::* };
+use bcheck::Record;
+
+
+#[derive(Parser)]
+pub struct Import {
+
+    #[clap(default_value = "~/.checkbook/register.db")]
+    pub file_path: String,
+
+    #[clap(long, short)]
+    pub input_file: String
+}
+
+impl Import {
+    pub fn run(&self) {
+        copy_database_if_not_exists(&self.file_path);
+        self.import();
+    }
+
+    fn import(&self) {
+        let source_path = real_path(&self.input_file);
+        let records = match source_path {
+            ref p if p.ends_with(".bcheck") => {
+                if let Ok(retrieved_records) = Record::from_file(&source_path) {
+                    retrieved_records
+                } else {
+                    vec![]
+                }
+            },
+            ref p if p.ends_with(".tsv") => {
+                if let Ok(retrieved_records) = Record::from_tsv_file(&source_path) {
+                    retrieved_records
+                } else {
+                    vec![]
+                }
+            },
+            _ => vec![]
+        };
+
+        add_records_to_db(&self.file_path, &records)
+    }
+}
+
