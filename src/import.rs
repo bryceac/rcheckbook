54c199bb65af618d2ce3b497cac13d5b102676c4
Bryce Campbell <tonyhawk2100@gmail.com>
2023-11-18 17:52:50 -0800

    keep fixing code for importing.

--- src/import.rs
+++ src/import.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
-use crate::records::Records;
-use crate::{ database::* shared::* };
+use crate::{ database::*, shared::* };
+use bcheck::Record;
 
 
 #[derive(Parser)]
@@ -15,10 +15,32 @@ pub struct Import {
 impl Import {
     pub fn run(&self) {
         copy_database_if_not_exists(&self.file_path);
+        self.import();
     }
 
     fn import(&self) {
         let source_path = real_path(&self.origin_file);
+        let records = match source_path {
+            ref p if p.ends_with(".bcheck") => {
+                if let Ok(retrieved_records) = Record::from_file(&source_path) {
+                    retrieved_records
+                } else {
+                    vec![]
+                }
+            },
+            ref p if p.ends_with(".tsv") => {
+                if let Ok(retrieved_records) = Record::from_tsv_file(&source_path) {
+                    retrieved_records
+                } else {
+                    vec![]
+                }
+            },
+            _ => vec![]
+        };
+
+        for record in records {
+            update_or_add_record(&self.file_path, &record);
+        }
     }
 }
 
