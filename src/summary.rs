6dbdc0ca1c1e3e5cc4ffa1bcf7e8ddb69a3fc3fa
Bryce Campbell <tonyhawk2100@gmail.com>
2024-11-30 10:24:01 -0800

    fix some code.

--- src/summary.rs
+++ src/summary.rs
@@ -16,9 +16,18 @@ impl Summary {
     pub fn run(&self) {
         copy_database_if_not_exists(&self.file_path);
         let mut record_store = Records::from(load_records_from_db(&self.file_path));
+        let mut categories = load_categories_from_db(&self.file_path);
 
         match self.period {
-
+            Period::Week => {},
+            Period::Month => {},
+            Period::HalfYear => {},
+            Period::Year => {},
+            Period::All => {}
         }
+    }
+
+    fn display(records: &Vec<Record>, categories: &Vec<String>, period: &Period) {
+        println!("")
     }
 }
