05cd9c117d4da3b5c9ab71eaa73ed92a409f693f
Bryce Campbell <tonyhawk2100@gmail.com>
2024-11-30 09:47:21 -0800

    bring over code from playground.

---
+++ src/date_range.rs
@@ -0,0 +1,28 @@
+use chrono::prelude::*; // 0.4.38
+use chrono::{ Days, Duration };
+use std::mem;
+
+struct DateRange(DateTime<Local>, DateTime<Local>);
+
+impl Iterator for DateRange {
+    type Item = DateTime<Local>;
+    
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.0 <= self.1 {
+            let next = self.0 + Duration::days(1);
+            Some(mem::replace(&mut self.0, next))
+        } else {
+            None
+        }
+    }
+}
+
+impl DateRange {
+    fn contains(&self, date: DateTime<Local>) -> bool {
+        if date.timestamp() >= self.0.timestamp() && date.timestamp() <= self.1.timestamp() {
+            true
+        } else {
+            false
+        }
+    }
+}
