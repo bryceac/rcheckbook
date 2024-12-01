080099a6e28ede6f8f18a2360d520b3a1d3e025a
Bryce Campbell <tonyhawk2100@gmail.com>
2024-12-01 08:20:43 -0800

    get rid of unnecessary function.

--- src/list.rs
+++ src/list.rs
@@ -11,7 +11,13 @@ pub struct List {
     pub file_path: String,
 
     #[clap(long, short)]
-    pub category: Option<String>
+    pub category: Option<String>,
+
+    #[clap(long, short)]
+    pub vendor: Option<String>,
+
+    #[clap(long, short)]
+    pub memo: Option<String>
 }
 
 impl List {
@@ -19,23 +25,12 @@ impl List {
         copy_database_if_not_exists(&self.file_path);
         let mut record_store = Records::from(load_records_from_db(&self.file_path));
 
-        if let Some(_) = self.category {
-            record_store.filtered_display(self.category.clone(), &self.file_path)
-        } else {
-            record_store.display(&self.file_path);
-        }
+        display(record_store, category, &self.file_path)
         
     }
 }
 
-pub fn display(store: &Records, db: &str) {
-    for record in store.sorted_records() {
-        let balance = store.balance_for_record(db, &record);
-        println!("{}\t{:.2}", record, balance);
-    }
-}
-
-pub fn filtered_display(store: &Records, category: Option<String>, db: &str) {
+pub fn display(store: &Records, category: Option<String>, db: &str) {
     if let Some(category) = category {
         let filtered_records: Vec<Record> = store.sorted_records().into_iter().filter(|record| record.transaction.category.clone().unwrap_or("Uncategorized".to_string()).to_lowercase() == category.to_lowercase()).collect();
 
