cb48ba8e02fda9b5d651793666166be9e015efee
Bryce Campbell <tonyhawk2100@gmail.com>
2025-03-09 09:50:25 -0700

    increase version number.

--- src/rcheckbook.rs
+++ src/rcheckbook.rs
@@ -2,7 +2,7 @@ use clap::Parser;
 use crate::subcommand::SubCommand;
 
 #[derive(Parser)]
-#[clap(version = "0.3.0", author = "Bryce Campbell <tonyhawk2100@gmail.com>")]
+#[clap(version = "0.4.2", author = "Bryce Campbell <tonyhawk2100@gmail.com>")]
 pub struct RCheckbook {
 
     #[clap(subcommand)]
