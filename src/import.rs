3b3789763416a6ec8842a7bda3258e05e393bfb5
Bryce Campbell <tonyhawk2100@gmail.com>
2025-06-08 09:15:48 -0700

    work on allowing ods imports.

--- src/import.rs
+++ src/import.rs
@@ -1,7 +1,10 @@
+use std::path::Path;
+
 use clap::Parser;
 use crate::{ database::*, shared::* };
 use bcheck::{ Record, Transaction, TransactionType };
 use qif::{ DateFormat, QIF, Transaction as QIFTransaction, TransactionStatus, Type as QIFType };
+use spsheet::{ Value, ods, Sheet };
 
 
 #[derive(Parser)]
@@ -42,6 +45,9 @@ impl Import {
                     vec![]
                 }
             },
+            ref p if p.ends_with(".ods") => {
+                records_from_ods(p)
+            }
             _ => vec![]
         };
 
@@ -121,5 +127,150 @@ fn records_from_section(qif: QIF, section: QIFType) -> Vec<Record> {
             vec![]
         },
     }
+}
+
+fn record_from_row(row_index: usize, sheet: Sheet) -> Option<Record> {
+    let id = if let Some(cell) = sheet.get_cell(row_index, 0) {
+        if let Value::Str(id) = cell.get_value() {
+            Some(id.to_owned())
+        } else {
+            None
+        }
+    } else {
+        None
+    };
+
+    let date = if let Some(cell) = sheet.get_cell(row_index, 1) {
+        if let Value::Date(date) = cell.get_value() {
+            Some(date)
+        } else {
+            None
+        }
+    } else {
+        None
+    };
+
+    let check_number = if let Some(cell) = sheet.get_cell(row_index, 2) {
+        if let Value::Str(check_number) = cell.get_value() {
+            if let Ok(number) = check_number.parse::<u32>() {
+                Some(number)
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    } else {
+        None
+    };
+
+    let is_reconciled = if let Some(cell) = sheet.get_cell(row_index, 3) {
+        if let Value::Str(is_reconciled) = cell.get_value() {
+            is_reconciled.to_uppercase() == "Y"
+        } else {
+            false
+        }
+    } else {
+        false
+    };
+
+    let category = if let Some(cell) = sheet.get_cell(row_index, 4) {
+        if let Value::Str(category) = cell.get_value() {
+            if category.is_empty() {
+                None
+            } else {
+                Some(category.as_str())
+            }
+        } else {
+            None
+        }
+    } else {
+        None
+    };
+
+    let vendor = if let Some(cell) = sheet.get_cell(row_index, 5) {
+        if let Value::Str(vendor) = cell.get_value() {
+            if vendor.is_empty() {
+                None
+            } else {
+                Some(vendor.as_str())
+            }
+        } else {
+            None
+        }
+    } else {
+        None
+    };
+
+    let memo = if let Some(cell) = sheet.get_cell(row_index, 6) {
+        if let Value::Str(memo) = cell.get_value() {
+            Some(memo.as_str())
+        } else {
+            None
+        }
+    } else {
+        None
+    };
+
+    let mut transaction_type: Option<TransactionType> = None;
+
+    let amount = if let Some(cell) = sheet.get_cell(row_index, 7) {
+        if let Value::Float(amount) = cell.get_value() {
+            transaction_type = Some(TransactionType::Deposit);
+            amount.to_owned()
+        } else {
+            0.0
+        }
+    } else if let Some(cell) = sheet.get_cell(row_index, 8) {
+        if let Value::Float(amount) = cell.get_value() {
+            transaction_type = Some(TransactionType::Withdrawal);
+            amount.to_owned()
+        } else {
+            0.0
+        }
+    } else {
+        0.0
+    };
+
+    if let None = date {
+        None
+    } else if let None = vendor {
+        None
+    } else {
+        let date_string = if let Some(date) = date {
+            Some(date.format("%Y-%m-%d").to_string())
+        } else {
+            None
+        };
+        let transaction = Transaction::from(
+            date_string.as_deref(), 
+            check_number, 
+            category, 
+            vendor.unwrap(), 
+            memo.unwrap_or(""), 
+            amount, 
+            transaction_type.unwrap_or(TransactionType::Withdrawal), 
+            is_reconciled);
+
+        Some(Record::from(id.as_deref().unwrap_or(""), transaction.unwrap()))
+    }
+}
+
+fn records_from_ods(p: &str) -> Vec<Record> {
+    let mut records: Vec<Record> = vec![];
+
+    if let Ok(book) = ods::read(&Path::new(p)) {
+        let sheet = book.get_sheet(0);
+
+        let number_of_rows = sheet.get_rows().len();
+
+        for row_index in 0..number_of_rows {
+            if let Some(record) = record_from_row(row_index, sheet) {
+                records.push(record)
+            }
+        }
+    }
+
+    records
 }
 
