674a0d1285c59d5c06fd3e844645e1e1baa92cb6
Bryce Campbell <tonyhawk2100@gmail.com>
2025-07-19 15:07:06 -0700

    add another error for date format.

--- src/errors.rs
+++ src/errors.rs
@@ -1,12 +1,14 @@
 use std::fmt;
 
 pub enum ImportError {
+    InvalidDateFormat,
     TransactionTypeParsingError
 }
 
 impl ImportError {
     pub fn to_string(&self) -> String {
         match self {
+            Self::InvalidDateFormat => "Date must be in the YYYY-MM-DD format.".to_owned(),
             Self::TransactionTypeParsingError => "Cannot determine transaction type. Please make sure there is a value in only either the credit or withdrawal column".to_owned()
         }
     }
